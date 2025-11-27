02e0c3a65c65d5ac3c870dc67d5c37f8607678e5
Abner Oliveira <abner.santos@bdsdatasolution.com.br>
2025-11-26 22:10:34 -0300

    feat: Update Role entity to include createdAt parameter and fix seed date for roles

---
+++ src/External/Database/Migrations/20251127010937_SeedDateFixed.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sphera.API.External.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDateFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(528), new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(761) });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(1270), new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(1271) });
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: (short)3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(1273), new DateTime(2025, 11, 26, 4, 14, 7, 118, DateTimeKind.Utc).AddTicks(1273) });
+        }
+    }
+}
