4814c2f4ed3395401313f071ce4600f982e02995
AlanEdward19 <alan-edward@outlook.com.br>
2025-12-09 20:24:44 -0300

    Adiciona BillingModule para configuração de dependências
    Inclui o arquivo BillingModule.cs com método de extensão para registrar dependências de BillingEntries, ClientServicePrices e Invoices, facilitando a configuração dos serviços de faturamento no projeto.

---
+++ src/Billing/BillingModule.cs
@@ -0,0 +1,18 @@
+﻿using Sphera.API.Billing.BillingEntries;
+using Sphera.API.Billing.ClientServicePrices;
+using Sphera.API.Billing.Invoices;
+
+namespace Sphera.API.Billing;
+
+public static class BillingModule
+{
+    public static IServiceCollection ConfigureBillingRelatedDependencies(this IServiceCollection services)
+    {
+        services
+            .ConfigureBillingEntriesRelatedDependencies()
+            .ConfigureClientServicePricesRelatedDependencies()
+            .ConfigureInvoicesRelatedDependencies();
+
+        return services;
+    }
+}
