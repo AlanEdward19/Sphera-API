cca482b1c4d3391a0d4da240a79bf4a7cc1fa1c4
AlanEdward19 <alan-edward@outlook.com.br>
2025-12-09 20:25:07 -0300

    Mapeamento EF para Billing, Invoice e ClientServicePrice
    Adicionados arquivos de mapeamento para BillingEntry, ClientServicePrice, Invoice e InvoiceItem. Registrados novos DbSets e configurações no SpheraDbContext para suportar as entidades e seus relacionamentos no banco de dados.

---
+++ src/External/Database/Maps/InvoiceItemMap.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sphera.API.Billing.Invoices;
+
+namespace Sphera.API.External.Database.Maps;
+
+public class InvoiceItemMap : IEntityTypeConfiguration<InvoiceItem>
+{
+    public void Configure(EntityTypeBuilder<InvoiceItem> b)
+    {
+        b.ToTable("InvoiceItems");
+        b.HasKey(x => x.Id);
+
+        b.Property(x => x.Id)
+            .HasColumnType("uniqueidentifier")
+            .HasDefaultValueSql("NEWID()");
+
+        b.Property(x => x.InvoiceId)
+            .HasColumnType("uniqueidentifier")
+            .IsRequired();
+
+        b.Property(x => x.ServiceId)
+            .HasColumnType("uniqueidentifier");
+
+        b.Property(x => x.Description)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        b.Property(x => x.Quantity)
+            .HasColumnType("decimal(18,4)")
+            .IsRequired();
+
+        b.Property(x => x.UnitPrice)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        b.Property(x => x.AdditionalAmount)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        b.Property(x => x.TotalAmount)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        b.Property(x => x.IsAdditional)
+            .IsRequired();
+
+        b.HasIndex(x => x.InvoiceId)
+            .HasDatabaseName("IX_InvoiceItems_InvoiceId");
+
+        b.HasIndex(x => x.ServiceId)
+            .HasDatabaseName("IX_InvoiceItems_ServiceId");
+
+        b.HasOne(x => x.Invoice)
+            .WithMany(i => i.Items)
+            .HasForeignKey(x => x.InvoiceId)
+            .HasConstraintName("FK_InvoiceItems_Invoices")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        b.HasOne(x => x.Service)
+            .WithMany()
+            .HasForeignKey(x => x.ServiceId)
+            .HasConstraintName("FK_InvoiceItems_Services")
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
