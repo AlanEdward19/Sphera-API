912a3e593c552beede0d93606cd4bbfad0e0b953
Pedro Tejon <pedrohtejon@gmail.com>
2026-01-13 01:33:05 +0000

    feat: download remittance route

--- src/Billing/Remittances/DownloadRemittanceFile/DownloadRemittanceFileCommandHandler.cs
+++ src/Billing/Remittances/DownloadRemittanceFile/DownloadRemittanceFileCommandHandler.cs
@@ -1,11 +1,49 @@
-﻿using Sphera.API.Shared.Interfaces;
+﻿using System.Data.Entity;
+using Sphera.API.External.Database;
+using Sphera.API.Shared;
+using Sphera.API.Shared.DTOs;
+using Sphera.API.Shared.Interfaces;
+using Sphera.API.Shared.Utils;
 
 namespace Sphera.API.Billing.Remittances.DownloadRemittanceFile;
 
-public class DownloadRemittanceFileCommandHandler([FromKeyedServices("billing")] IStorage storage) : IHandler<DownloadRemittanceFileCommand, (Stream, string)>
+public class DownloadRemittanceFileCommandHandler(SpheraDbContext dbContext, [FromKeyedServices("billing")] IStorage storage, Logger<DownloadRemittanceFileCommandHandler> logger) : IHandler<DownloadRemittanceFileCommand, (Stream, string)>
 {
-    public Task<IResultDTO<(Stream, string)>> HandleAsync(DownloadRemittanceFileCommand request, HttpContext context, CancellationToken cancellationToken)
+    public async Task<IResultDTO<(Stream, string)>> HandleAsync(DownloadRemittanceFileCommand request, HttpContext context, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        logger.LogInformation("Baixando arquivo de billet {Id}", request.Id);
+
+        var userId = context.User.GetUserId();
+        if (userId == Guid.Empty)
+            return ResultDTO<(Stream, string)>.AsFailure(new FailureDTO(401, "Unauthorized"));
+        
+        var entity = await dbContext.Remittances
+            .AsNoTracking()
+            .FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken);
+
+        if (entity is null)
+            return ResultDTO<(Stream, string)>.AsFailure(new FailureDTO(404, "Remittance not found"));
+        
+        try
+        {
+            var fileName =
+                $"remittances/{entity.FileName}.rem";
+                
+            if (await storage.ExistsAsync(fileName, cancellationToken)) 
+            {
+                var result = await storage.DownloadAsync(fileName, cancellationToken);
+                return ResultDTO<(Stream, string)>.AsSuccess((result, $"{entity.FileName}.rem"));
+            }
+            else 
+                return ResultDTO<(Stream, string)>.AsFailure(new FailureDTO(404, "Remessa não encontrado"));
+        }
+        catch (DomainException ex)
+        {
+            return ResultDTO<(Stream, string)>.AsFailure(new FailureDTO(400, ex.Message));
+        }
+        catch (Exception)
+        {
+            return ResultDTO<(Stream, string)>.AsFailure(new FailureDTO(500, "Erro ao baixar remessa."));
+        }
     }
 }
